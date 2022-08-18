5e6d3f595cbbfdc0d5b943e0228885fe476012cd
prasanthki95 <prasanth.kumar@i95dev.com>
2022-08-18 20:41:56 +0530

    New files added

---
+++ CronJob/Cron/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace I95Learning\CronJob\Cron;
+
+class Test
+{
+
+    public function execute()
+    {
+
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/cron.log');
+        $logger = new \Zend\Log\Logger();
+        $logger->addWriter($writer);
+        $logger->info(__METHOD__);
+
+        return $this;
+
+    }
+}
+
